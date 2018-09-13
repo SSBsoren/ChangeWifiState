9160ff00a42ce0fb798f210039ea6f0550012092
saGen <sagensakam015@gmail.com>
2018-09-13 10:10:49 +0530

    Done

--- app/src/main/java/com/soren/sagen/fragmentactivity/MainActivity.java
+++ app/src/main/java/com/soren/sagen/fragmentactivity/MainActivity.java
@@ -1,13 +1,49 @@
 package com.soren.sagen.fragmentactivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button offBtn,onBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        onBtn = findViewById(R.id.turnon_btn);
+        offBtn = findViewById(R.id.turnoff_btn);
+
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(true);
+                Toast.makeText(getApplicationContext(),"WiFi is on",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        offBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifiManager.setWifiEnabled(false);
+                Toast.makeText(getApplicationContext(),"WiFi is off",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
     }
 }
